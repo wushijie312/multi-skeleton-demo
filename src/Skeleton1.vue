c6562e5a6dbe42887cdcfb4f12b7f63ee9b65bde
wushijie312 <1281310367@qq.com>
2019-06-20 15:16:50 +0800

    's'

--- src/Skeleton1.vue
+++ src/Skeleton1.vue
@@ -1,5 +1,11 @@
 <template>
-    <div class="skeleton-wrapper">Skeleton For Home</div>
+   <div class="skeleton">
+    <div class="skeleton-head"></div>
+    <div class="skeleton-body">
+        <div class="skeleton-title"></div>
+        <div class="skeleton-content"></div>
+    </div>
+</div>
 </template>
 
 <script>
@@ -11,18 +17,48 @@ export default {
 
 <style scoped>
 
-.skeleton-header {
-    height: 152px;
-    background: grey;
-    margin-top: 60px;
-    width: 152px;
-    margin: 60px auto;
+.skeleton {
+  padding: 10px;
 }
 
-.skeleton-block {
-    display: flex;
-    flex-direction: column;
-    padding-top: 8px;
+.skeleton .skeleton-head,
+.skeleton .skeleton-title,
+.skeleton .skeleton-content {
+  background: rgb(194, 207, 214);
+}
+
+.skeleton-head {
+  width: 100px;
+  height: 100px;
+  float: left;
+}
+
+.skeleton-body {
+  margin-left: 110px;
+}
+
+.skeleton-title {
+  width: 500px;
+  height: 60px;
+  transform-origin: left;
+  animation: skeleton-stretch .5s linear infinite alternate;
+}
+
+.skeleton-content {
+  width: 260px;
+  height: 30px;
+  margin-top: 10px;
+  transform-origin: left;
+  animation: skeleton-stretch .5s -.3s linear infinite alternate;
+}
+
+@keyframes skeleton-stretch {
+  from {
+    transform: scalex(1);
+  }
+  to {
+    transform: scalex(.3);
+  }
 }
 
 </style>
